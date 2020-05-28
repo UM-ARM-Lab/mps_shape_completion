f797125266ff1099f1a97bd08eff10fe10f9cd4c
Peter Mitrano <pmitrano@umich.edu>
2020-05-27 21:33:06 -0400

    implement save best & latest seperately

--- shape_completion_training/src/shape_completion_training/model_runner.py
+++ shape_completion_training/src/shape_completion_training/model_runner.py
@@ -1,5 +1,6 @@
 from colorama import Style, Fore
 
+from shape_completion_training.metric import LossMetric
 from shape_completion_training.model import utils
 from shape_completion_training.model.utils import reduce_mean_dict, sequence_of_dicts_to_dict_of_sequences
 
@@ -63,19 +64,34 @@ class ModelRunner:
         self.num_train_batches = None
         self.num_val_batches = None
 
-        self.ckpt = tf.train.Checkpoint(step=tf.Variable(1),
-                                        epoch=tf.Variable(0),
-                                        train_time=tf.Variable(0.0),
-                                        model=self.model)
-        self.checkpoint_path = self.trial_path / "training_checkpoints/"
-        self.manager = tf.train.CheckpointManager(self.ckpt, self.checkpoint_path.as_posix(), max_to_keep=1)
-        self.restore()
+        self.latest_ckpt = tf.train.Checkpoint(step=tf.Variable(1),
+                                               epoch=tf.Variable(0),
+                                               train_time=tf.Variable(0.0),
+                                               model=self.model)
+        self.best_ckpt = tf.train.Checkpoint(step=tf.Variable(1),
+                                             epoch=tf.Variable(0),
+                                             train_time=tf.Variable(0.0),
+                                             model=self.model)
+        self.latest_checkpoint_path = self.trial_path / "latest_checkpoint"
+        self.best_checkpoint_path = self.trial_path / "best_checkpoint"
+        self.latest_checkpoint_manager = tf.train.CheckpointManager(self.latest_ckpt, self.latest_checkpoint_path.as_posix(),
+                                                                    max_to_keep=1)
+        self.best_checkpoint_manager = tf.train.CheckpointManager(self.best_ckpt, self.best_checkpoint_path.as_posix(),
+                                                                  max_to_keep=1)
+        self.restore_best()
 
-    def restore(self):
-        status = self.ckpt.restore(self.manager.latest_checkpoint)
+    def restore_best(self):
+        status = self.best_ckpt.restore(self.best_checkpoint_manager.latest_checkpoint)
+
+        # Suppress warning
+        if self.latest_checkpoint_manager.latest_checkpoint:
+            status.assert_existing_objects_matched()
+
+    def restore_latest(self):
+        status = self.latest_ckpt.restore(self.latest_checkpoint_manager.latest_checkpoint)
 
         # Suppress warning 
-        if self.manager.latest_checkpoint:
+        if self.latest_checkpoint_manager.latest_checkpoint:
             status.assert_existing_objects_matched()
 
     def count_params(self):
@@ -86,7 +102,7 @@ class ModelRunner:
         tf.summary.trace_on(graph=True, profiler=False)
         self.model(elem, training=True)
         with self.train_summary_writer.as_default():
-            tf.summary.trace_export(name='train_trace', step=self.ckpt.step.numpy())
+            tf.summary.trace_export(name='train_trace', step=self.latest_ckpt.step.numpy())
 
         model_image_path = self.trial_path / 'network.png'
         tf.keras.utils.plot_model(self.model, model_image_path.as_posix(), show_shapes=True)
@@ -94,12 +110,12 @@ class ModelRunner:
     def write_train_summary(self, summary_dict):
         with self.train_summary_writer.as_default():
             for k in summary_dict:
-                tf.summary.scalar(k, summary_dict[k].numpy(), step=self.ckpt.step.numpy())
+                tf.summary.scalar(k, summary_dict[k].numpy(), step=self.latest_ckpt.step.numpy())
 
     def write_val_summary(self, summary_dict):
         with self.val_summary_writer.as_default():
             for k in summary_dict:
-                tf.summary.scalar(k, summary_dict[k].numpy(), step=self.ckpt.step.numpy())
+                tf.summary.scalar(k, summary_dict[k].numpy(), step=self.latest_ckpt.step.numpy())
 
     def train_epoch(self, train_dataset, val_dataset):
         if self.num_train_batches is not None:
@@ -120,18 +136,14 @@ class ModelRunner:
             t0 = time.time()
             for train_batch in train_dataset:
                 self.num_train_batches += 1
-                self.ckpt.step.assign_add(1)
+                self.latest_ckpt.step.assign_add(1)
 
                 _, train_batch_metrics = self.model.train_step(train_batch)
-                time_str = str(datetime.timedelta(seconds=int(self.ckpt.train_time.numpy())))
+                time_str = str(datetime.timedelta(seconds=int(self.latest_ckpt.train_time.numpy())))
                 bar.update(self.num_train_batches, Loss=train_batch_metrics['loss'].numpy().squeeze(), TrainTime=time_str)
                 self.write_train_summary(train_batch_metrics)
-                self.ckpt.train_time.assign_add(time.time() - t0)
+                self.latest_ckpt.train_time.assign_add(time.time() - t0)
                 t0 = time.time()
-
-        save_path = self.manager.save()
-        print(Fore.CYAN + "Saved checkpoint for step {}: {}".format(int(self.ckpt.step), save_path) + Fore.RESET)
-        print("train loss {:1.3f}".format(train_batch_metrics['loss'].numpy()))
 
     def val_epoch(self, val_dataset):
         if self.num_val_batches is not None:
@@ -154,25 +166,34 @@ class ModelRunner:
                 val_metrics.append(val_batch_metrics)
                 bar.update(self.num_val_batches)
 
-            val_metrics = sequence_of_dicts_to_dict_of_sequences(val_metrics)
-            mean_val_metrics = reduce_mean_dict(val_metrics)
-            self.write_val_summary(mean_val_metrics)
+        val_metrics = sequence_of_dicts_to_dict_of_sequences(val_metrics)
+        mean_val_metrics = reduce_mean_dict(val_metrics)
+        self.write_val_summary(mean_val_metrics)
         print(Style.BRIGHT + "val loss {:1.3f}".format(mean_val_metrics['loss'].numpy()) + Style.NORMAL)
+        return mean_val_metrics
 
-    def train(self, train_dataset, val_dataset, num_epochs, seed):
+    def train(self, train_dataset, val_dataset, num_epochs, key_metric=LossMetric):
         self.build_model(train_dataset)
         self.count_params()
 
+        best_key_metric_value = None
         try:
-            while self.ckpt.epoch < num_epochs:
+            while self.latest_ckpt.epoch < num_epochs:
                 # Training
-                self.ckpt.epoch.assign_add(1)
+                self.latest_ckpt.epoch.assign_add(1)
                 print('')
-                msg_fmt = Fore.GREEN + Style.BRIGHT + 'Epoch {:3d}/{}, Group Name {}' + Style.RESET_ALL
-                print(msg_fmt.format(self.ckpt.epoch.numpy(), num_epochs, self.group_name))
+                msg_fmt = Fore.GREEN + Style.BRIGHT + 'Epoch {:3d}/{}, Group Name [{}]' + Style.RESET_ALL
+                print(msg_fmt.format(self.latest_ckpt.epoch.numpy(), num_epochs, self.group_name))
                 self.train_epoch(train_dataset, val_dataset)
-
+                self.latest_checkpoint_manager.save()
                 # Validation at end of epoch
-                self.val_epoch(val_dataset)
+                valdation_metrics = self.val_epoch(val_dataset)
+
+                key_metric_value = valdation_metrics[key_metric.key()]
+                if best_key_metric_value is None or key_metric.is_better_than(key_metric_value, best_key_metric_value):
+                    best_key_metric_value = key_metric_value
+                    save_path = self.best_checkpoint_manager.save()
+                    print(Fore.CYAN + "New best checkpoint {}".format(save_path) + Fore.RESET)
+
         except KeyboardInterrupt:
             print(Fore.YELLOW + "Interrupted." + Fore.RESET)
--- shape_completion_training/src/shape_completion_training/tests/test_model_runner.py
+++ shape_completion_training/src/shape_completion_training/tests/test_model_runner.py
@@ -7,27 +7,7 @@ from shape_completion_training.model_runner import ModelRunner
 from shape_completion_training.mykerasmodel import MyKerasModel
 
 params = {
-    'num_latent_layers': 200,
-    'translation_pixel_range_x': 10,
-    'translation_pixel_range_y': 10,
-    'translation_pixel_range_z': 10,
-    'is_u_connected': False,
-    'final_activation': 'None',
-    'unet_dropout_rate': 0.5,
-    'use_final_unet_layer': False,
-    'simulate_partial_completion': False,
-    'simulate_random_partial_completion': False,
-    # 'network': 'VoxelCNN',
-    # 'network': 'VAE_GAN',
-    # 'network': 'Augmented_VAE',
-    # 'network': 'Conditional_VCNN',
-    'network': 'AE_VCNN',
-    'stacknet_version': 'v2',
-    'turn_on_prob': 0.00000,
-    'turn_off_prob': 0.0,
-    'loss': 'cross_entropy',
-    'multistep_loss': False,
-    'learning_rate': 0.001,
+    'learning_rate': 0.1,
 }
 
 
@@ -53,17 +33,44 @@ class FakeModel(MyKerasModel):
 
 
 class ModelRunnerTraining(unittest.TestCase):
+    @staticmethod
+    def simple_dataset(a, b):
+        numbers = tf.data.Dataset.range(a, b).map(lambda x: tf.cast(x, tf.float32))
+        x = numbers.map(lambda x: {'x': x}).batch(1).batch(64)
+        y = numbers.map(lambda x: {'y': x * 2 + 2}).batch(1).batch(64)
+        dataset = tf.data.Dataset.zip((x, y))
+        return dataset
+
     @classmethod
     def setUpClass(cls):
-        numbers = tf.data.Dataset.range(1000).map(lambda x: tf.cast(x, tf.float32))
-        x = numbers.map(lambda x: {'x': x}).batch(1).batch(16)
-        y = numbers.map(lambda x: {'y': x * 5 + 2}).batch(1).batch(16)
-        cls.dataset = tf.data.Dataset.zip((x, y))
+        cls.dataset = ModelRunnerTraining.simple_dataset(0, 100)
+        cls.val_dataset = ModelRunnerTraining.simple_dataset(200, 300)
 
     def test_train(self):
         model = FakeModel(params=params)
         mr = ModelRunner(model=model, training=True, params=params, write_summary=False)
-        mr.train(ModelRunnerTraining.dataset, ModelRunnerTraining.dataset, num_epochs=1, seed=1)
+        mr.train(ModelRunnerTraining.dataset, ModelRunnerTraining.val_dataset, num_epochs=1)
+
+    def test_save_latest_and_best(self):
+        model = FakeModel(params=params)
+        mr = ModelRunner(model=model, group_name='latest_and_best', training=True, params=params, write_summary=False)
+        mr.train(ModelRunnerTraining.dataset, ModelRunnerTraining.val_dataset, num_epochs=100)
+
+        latest_checkpoint_dir = mr.trial_path / 'latest_checkpoint' / 'checkpoint'
+        best_checkpoint_dir = mr.trial_path / 'best_checkpoint' / 'checkpoint'
+        self.assertTrue(latest_checkpoint_dir.exists())
+        self.assertTrue(best_checkpoint_dir.exists())
+
+        latest_checkpoint = mr.latest_checkpoint_manager.latest_checkpoint
+        latest_ckpt = tf.train.Checkpoint(step=tf.Variable(1))
+        latest_ckpt.restore(latest_checkpoint).expect_partial()
+        latest_checkpoint_step = latest_ckpt.step.numpy()
+
+        best_checkpoint = mr.best_checkpoint_manager.latest_checkpoint
+        best_ckpt = tf.train.Checkpoint(step=tf.Variable(1))
+        best_ckpt.restore(best_checkpoint).expect_partial()
+        best_checkpoint_step = best_ckpt.step.numpy()
+        self.assertLess(best_checkpoint_step, latest_checkpoint_step)
 
 
 if __name__ == '__main__':
