a3e38bd311a106abf38b5722c9f87a04e1cf03d3
Peter Mitrano <pmitrano@umich.edu>
2020-06-01 13:29:17 -0400

    tweaking restore

--- shape_completion_training/src/shape_completion_training/model_runner.py
+++ shape_completion_training/src/shape_completion_training/model_runner.py
@@ -45,7 +45,9 @@ class ModelRunner:
                  trial_path=None,
                  params=None,
                  trials_directory=None,
+                 restore_from_best=False,
                  write_summary=True):
+        self.restore_from_best = restore_from_best
         self.model = model
         self.side_length = 64
         self.num_voxels = self.side_length ** 3
@@ -67,10 +69,12 @@ class ModelRunner:
         self.latest_ckpt = tf.train.Checkpoint(step=tf.Variable(1),
                                                epoch=tf.Variable(0),
                                                train_time=tf.Variable(0.0),
+                                               best_key_metric_value=tf.Variable(10000.0),
                                                model=self.model)
         self.best_ckpt = tf.train.Checkpoint(step=tf.Variable(1),
                                              epoch=tf.Variable(0),
                                              train_time=tf.Variable(0.0),
+                                             best_key_metric_value=tf.Variable(10000.0),
                                              model=self.model)
         self.latest_checkpoint_path = self.trial_path / "latest_checkpoint"
         self.best_checkpoint_path = self.trial_path / "best_checkpoint"
@@ -78,17 +82,26 @@ class ModelRunner:
                                                                     max_to_keep=1)
         self.best_checkpoint_manager = tf.train.CheckpointManager(self.best_ckpt, self.best_checkpoint_path.as_posix(),
                                                                   max_to_keep=1)
-        self.restore_best()
+
+        if self.restore_from_best:
+            self.restore_best()
+        else:
+            self.restore_latest()
 
     def restore_best(self):
         status = self.best_ckpt.restore(self.best_checkpoint_manager.latest_checkpoint)
+        if self.best_checkpoint_manager.latest_checkpoint is not None:
+            print(Fore.CYAN + "Restoring best {}".format(self.best_checkpoint_manager.latest_checkpoint) + Fore.RESET)
+            self.latest_ckpt = self.best_ckpt
 
         # Suppress warning
-        if self.latest_checkpoint_manager.latest_checkpoint:
+        if self.best_checkpoint_manager.latest_checkpoint:
             status.assert_existing_objects_matched()
 
     def restore_latest(self):
         status = self.latest_ckpt.restore(self.latest_checkpoint_manager.latest_checkpoint)
+        if self.latest_checkpoint_manager.latest_checkpoint is not None:
+            print(Fore.CYAN + "Restoring latest {}".format(self.latest_checkpoint_manager.latest_checkpoint) + Fore.RESET)
 
         # Suppress warning 
         if self.latest_checkpoint_manager.latest_checkpoint:
@@ -169,29 +182,33 @@ class ModelRunner:
         val_metrics = sequence_of_dicts_to_dict_of_sequences(val_metrics)
         mean_val_metrics = reduce_mean_dict(val_metrics)
         self.write_val_summary(mean_val_metrics)
-        print(Style.BRIGHT + "val loss {:1.3f}".format(mean_val_metrics['loss'].numpy()) + Style.NORMAL)
+        print(Style.BRIGHT + "val loss {:9.4f}".format(mean_val_metrics['loss'].numpy()) + Style.NORMAL)
         return mean_val_metrics
 
     def train(self, train_dataset, val_dataset, num_epochs, key_metric=LossMetric):
         self.build_model(train_dataset)
         self.count_params()
 
-        best_key_metric_value = None
+        last_epoch = self.latest_ckpt.epoch + num_epochs
         try:
-            while self.latest_ckpt.epoch < num_epochs:
+            # Validation before anything
+            self.val_epoch(val_dataset)
+
+            while self.latest_ckpt.epoch < last_epoch:
                 # Training
                 self.latest_ckpt.epoch.assign_add(1)
                 print('')
                 msg_fmt = Fore.GREEN + Style.BRIGHT + 'Epoch {:3d}/{}, Group Name [{}]' + Style.RESET_ALL
-                print(msg_fmt.format(self.latest_ckpt.epoch.numpy(), num_epochs, self.group_name))
+                print(msg_fmt.format(self.latest_ckpt.epoch.numpy(), last_epoch, self.group_name))
                 self.train_epoch(train_dataset, val_dataset)
                 self.latest_checkpoint_manager.save()
+
                 # Validation at end of epoch
                 valdation_metrics = self.val_epoch(val_dataset)
-
                 key_metric_value = valdation_metrics[key_metric.key()]
-                if best_key_metric_value is None or key_metric.is_better_than(key_metric_value, best_key_metric_value):
-                    best_key_metric_value = key_metric_value
+                if key_metric.is_better_than(key_metric_value, self.best_ckpt.best_key_metric_value):
+                    self.best_ckpt.best_key_metric_value.assign(key_metric_value)
+                    self.latest_ckpt.best_key_metric_value.assign(key_metric_value)
                     save_path = self.best_checkpoint_manager.save()
                     print(Fore.CYAN + "New best checkpoint {}".format(save_path) + Fore.RESET)
 
